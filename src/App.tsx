6b1d9b4a9c1a29ff6ac9b3cfb717b5872e1f7ea4
sasha💜 <zaitseva@spiks.ru>
2022-04-12 23:29:41 +0300

    add rectangular that you can move with your keyboard arrows add a check if it is a visible area of the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+const step = 25;
+
 function App() {
+  const [coords, setCoords] = useState<[number, number]>([0, 0]);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(
+    () => {
+      const listener = (event: KeyboardEvent) => {
+        const { current } = ref;
+        if (!current) {
+          return;
+        }
+
+        const boundingClientRect = current.getBoundingClientRect();
+        const { documentElement } = document;
+
+        switch (event.code) {
+          case 'ArrowUp': {
+            const diff = boundingClientRect.top - step;
+            if (diff > 0) {
+              setCoords((prevValue) => [prevValue[0], prevValue[1] - step]);
+            } else {
+              setCoords((prevValue) => [prevValue[0], prevValue[1] - step - diff]);
+            } break;
+          }
+
+          case 'ArrowDown': {
+            const documentHeight = documentElement.clientHeight;
+            const diff = documentHeight - (boundingClientRect.bottom + step);
+            if (diff > 0) {
+              setCoords((prevValue) => [prevValue[0], prevValue[1] + step]); break;
+            } else {
+              setCoords((prevValue) => [prevValue[0], prevValue[1] + step + diff]);
+            } break;
+          }
+
+          case 'ArrowRight': {
+            const documentWidth = documentElement.clientWidth;
+            const diff = documentWidth - (boundingClientRect.right + step);
+            if (diff > 0) {
+              setCoords((prevValue) => [prevValue[0] + step, prevValue[1]]);
+            } else {
+              setCoords((prevValue) => [prevValue[0] + step + diff, prevValue[1]]);
+            }
+            break;
+          }
+
+          case 'ArrowLeft': {
+            const diff = boundingClientRect.left - step;
+            if (diff > 0) {
+              setCoords((prevValue) => [prevValue[0] - step, prevValue[1]]);
+            } else {
+              setCoords((prevValue) => [prevValue[0] - step - diff, prevValue[1]]);
+            }
+            break;
+          }
+          default: break;
+        }
+      };
+
+      document.addEventListener('keydown', listener);
+
+      return () => { document.removeEventListener('keydown', listener); };
+    },
+    [],
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="car" ref={ref} style={{ transform: `translate(${coords[0]}px, ${coords[1]}px)` }} />
     </div>
   );
 }
